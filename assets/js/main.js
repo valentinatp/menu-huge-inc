07b26f6ab6932e5e146fd18e5b014c3e9de96301
valentinatp <valentina.toledoporma@gmail.com>
2017-05-11 23:24:46 -0300

    termino el ejercicio guiado

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,23 @@ window.addEventListener("scroll", function() {
     }
     lastScrollTop = currentScroll;
 });
+    else {
+        document.getElementById("nav-header").classList.add("header-in");
+        document.getElementById("nav-header").classList.add("solid");
+        document.getElementById("nav-header").style.opacity = 1;
+    }
+    if (currentScroll <= 3) {
+        document.getElementById("nav-header").classList.remove("solid");
+        document.getElementById("nav-header").classList.remove("header-in");
+    }
+    if (currentScroll > lastScrollTop) {
+    document.getElementById("nav-header").classList.remove("header-in");
+    document.getElementById("nav-header").style.opacity = 0;
+} else {
+    (lastScrollTop = currentScroll;
+    }, false);
+document.getElementById("toggle").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("header").classList.toggle("open");
+    document.getElementById('body').classList.toggle("overflow-hidden");
+});
