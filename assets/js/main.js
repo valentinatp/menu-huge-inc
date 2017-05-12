64265500192244941572fdfd6a55527bfba81c01
valentinatp <valentina.toledoporma@gmail.com>
2017-05-11 23:18:41 -0300

    agrego condicionales al 'js'

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,14 @@ document.getElementById("toggle").addEventListener("click", function(event) {
     document.getElementById("nav-header").classList.toggle("open");
     document.getElementById("body").classList.toggle("overflow-hidden");
 });
+var currentScrollTop = window.pageYOffsed || document.body.scrollTop;
+var lastScrollTop = 0;
+window.addEventListener("scroll", function() {
+   var currentScroll = window.pageYOffset || document.body.scrollTop;
+    if (currentScroll > lastScrollTop) {
+        console.log("down");
+    } else {
+        console.log("up");
+    }
+    lastScrollTop = currentScroll;
+});
